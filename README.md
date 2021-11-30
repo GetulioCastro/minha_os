73a12d1355271962981672215e76ced5194fede1
GetulioCastroJr <augustoge71@gmail.com>
2021-11-30 15:10:06 -0300

    finalizado

---
+++ README.md
@@ -0,0 +1,2 @@
+# minha_ordem_de_servico
+BOT para capturar as ordens de serviço de forma automática, quando eu estiver ocupado em outras atividades
---
+++ scrapping.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from time import sleep
+
+navegador = webdriver.Chrome()
+
+navegador.get('https://engeplusinfo.com/SAC/')
+sleep(1)
+
+#acessando a página do SAC
+navegador.find_element_by_xpath('//*[@id="txtEmpresa"]').send_keys('065') 
+
+navegador.find_element_by_xpath('//*[@id="txtUsuario"]').send_keys('getulio') 
+
+navegador.find_element_by_xpath('//*[@id="txtSenha"]').send_keys('Suporteweb#1')
+
+navegador.find_element_by_xpath('//*[@id="btEntrar"]').click() 
+sleep(0.5)
+
+#acessando o menu de OSs
+navegador.find_element_by_xpath('//*[@id="menuitem_Solicitacões"]/p').click()
+
+navegador.find_element_by_xpath('//*[@id="menuitemexpand_Solicitacões"]/a[1]').click()
+
+navegador.find_element_by_xpath('//*[@id="btFiltro"]').click()
+
+navegador.find_element_by_xpath('//*[@id="dropTipoBusca"]').click()
+
+navegador.find_element_by_xpath('//*[@id="dropStatus"]').click()
+
+navegador.find_element_by_xpath('/html/body/form/div[6]/div/div/div/div/div[2]/div[1]/div[1]/div/select/option[3]').click()
+
+navegador.find_element_by_xpath('//*[@id="dropModulo"]').click()
+
+navegador.find_element_by_xpath('//*[@id="dropModulo"]/option[14]').click()
+
+navegador.find_element_by_xpath('//*[@id="btBuscar"]').click()
+
+#finalizado
